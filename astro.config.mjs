927a915bce7bec6b3ea42ea35a7f55cfbd0b7d0f
Jankku <29043938+Jankku@users.noreply.github.com>
2025-07-07 14:17:24 +0300

    refactor: fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,46 @@ export default defineConfig({
     plugins: [tailwind()],
   },
   experimental: {
-    svg: true,
+    fonts: [
+      {
+        provider: 'local',
+        name: 'Cooper Hewitt',
+        cssVariable: '--font-cooper-hewitt',
+        variants: [
+          {
+            src: ['./src/fonts/CooperHewitt-Book.woff2'],
+            weight: 400,
+            style: 'normal',
+          },
+          {
+            src: ['./src/fonts/CooperHewitt-Medium.woff2'],
+            weight: 500,
+            style: 'normal',
+          },
+          {
+            src: ['./src/fonts/CooperHewitt-Semibold.woff2'],
+            weight: 600,
+            style: 'normal',
+          },
+          {
+            src: ['./src/fonts/CooperHewitt-Bold.woff2'],
+            weight: 700,
+            style: 'normal',
+          },
+        ],
+      },
+      {
+        provider: 'local',
+        name: 'Concourse-Index',
+        cssVariable: '--font-concourse-index',
+        variants: [
+          {
+            src: ['./src/fonts/Concourse-Index-Regular.woff2'],
+            weight: 'normal',
+            style: 'normal',
+          },
+        ],
+      },
+    ],
   },
 });
